838d79df3b6afa0113f611a0f2a8c2f759191021
Nelson Ramirez <zerocool23893@gmail>
2019-08-28 18:22:09 -0600

    Se agrego una plantilla Html se hizo un queryset sobre las publicacaciones ya publicadas

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+
+# Create your views here.
+def post_list(request):
+    return render(request, 'blog/listar_pub.html', {})
+
+
+    def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/listar_pub.html', {'posts': posts})
