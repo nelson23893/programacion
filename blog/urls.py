179cbac5f11e51a016057f28ec0a51856d99b70f
Nelson Ramirez <zerocool23893@gmail>
2019-09-04 18:26:43 -0600

    Se actualizo bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='listar_pub'),
+    path('',views.detalle_pub, name='detalle_pub'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 
 # Create your views here.
+
 def post_list(request):
-    return render(request, 'blog/listar_pub.html', {})
-
-
-    def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_pub.html', {'posts': posts})
+
+def detalle_pub(request,pk):
+	p = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/detalle_pub.html', {'p':p})
