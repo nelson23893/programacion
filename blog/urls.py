b30d3ec74aa5d51dc8efb0d4ded1ccc42b419ad9
Nelson Ramirez <zerocool23893@gmail>
2019-09-06 01:12:36 -0600

    url

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='listar_pub'),
-    path('',views.detalle_pub, name='detalle_pub'),
+    path('', views.detalle_pub, name='detalle_pub'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_pub.html', {'posts': posts})
 
-def detalle_pub(request,pk):
-	p = get_object_or_404(Post, pk=pk)
-	return render(request, 'blog/detalle_pub.html', {'p':p})
+def detalle_pub(request, pk):
+    p = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detalle_pub.html', {'p': p})
